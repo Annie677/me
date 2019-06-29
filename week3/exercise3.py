33d902ef04295b020437e3e1d4c004cef6225bf7
guanlin he <guanlinhe@guanlindeMacBook-Pro.local>
2019-06-30 00:11:23 +1000

    I cant find what goes wrong

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -7,7 +7,50 @@ import random
 
 
 def advancedGuessingGame():
-    """Play a guessing game with a user.
+  print("Welcome to the guessing game!")
+  print("Please guess a number between _ and _?")
+  lower_bound = input()
+  upper_bound = input()
+
+  while True:
+    try:
+      lower_bound = int(input("Enter a lower bound: "))
+      upper_bound = int(input("Enter an upper bound: "))
+      print("Great, a number between {lower} and {upper}.".format(lower = lower_bound, upper = upper_bound))
+      break
+    except:
+      lower_bound = input("Enter a lower bound: ")
+      upper_bound = input("Enter an upper bound: ")
+      print("Please give me two numbers.")
+
+  actual_number = random.randint(lower_bound, upper_bound)
+  guess_number = input()
+  guessed = False
+  message = "Try to guess a number."
+  
+  while not guessed:
+    print(message)
+    try:
+      guess_number = int(input(message))
+      print ("Your guessed number is {}".format(guess_number))
+      if guess_number == actual_number:
+        print("You got it! It was {}".format(actual_number))
+        guessed = True
+      elif guessedNumber < actualNumber:
+        print("Too small, try again :'(")
+      else:
+        print("Too big, try again :'(")
+
+    except:
+      guess_number = input(message)
+      print("{} is not a number, please give me a number.".format(guess_number))
+
+  return "You got it!"
+
+
+
+
+  """Play a guessing game with a user.
 
     The exercise here is to rewrite the exampleGuessingGame() function
     from exercise 3, but to allow for:
@@ -26,7 +69,7 @@ def advancedGuessingGame():
     purpose if you can!
     """
 
-    return "You got it!"
+    #return "You got it!"
     # the tests are looking for the exact string "You got it!". Don't modify that!
 
 
